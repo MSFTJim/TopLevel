1e0a8c9cc0378a157fb0c31f0198baf50a3845b2
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2021-06-03 00:16:25 +0000

    first top level attempt

---
+++ TopAPI/Program.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+var app = WebApplication.Create(args);
+
+app.MapGet("/", async http =>
+{
+        await http.Response.WriteAsync("Hello World!");
+});
+await app.RunAsync();
